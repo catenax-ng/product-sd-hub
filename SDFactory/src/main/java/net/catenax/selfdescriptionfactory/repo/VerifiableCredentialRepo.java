190274f4c8f0bc1ef43da65e44c47f4544f805f2
Daniil Pankratov <daniil.pankratov@t-systems.com>
2022-06-09 13:24:12 +0400

    fix

--- SDFactory/src/main/java/net/catenax/selfdescriptionfactory/repo/VerifiableCredentialRepo.java
+++ SDFactory/src/main/java/net/catenax/selfdescriptionfactory/repo/VerifiableCredentialRepo.java
@@ -7,13 +7,4 @@ import java.util.List;
 
 @Repository
 public interface VerifiableCredentialRepo extends JpaRepository<VCModel, String> {
-    List<VCModel> findAllByIdInAndCompanyNumberInAndHeadquarterCountryInAndLegalCountryInAndServiceProviderInAndSdTypeInAndBpnIn(List<String> id,
-                                                                                                                                 List<String> companyNumber,
-                                                                                                                                 List<String> headquarterCountry,
-                                                                                                                                 List<String> legalCountry,
-                                                                                                                                 List<String> serviceProvider,
-                                                                                                                                 List<String> sdType,
-                                                                                                                                 List<String> bpn);
-
-    List<VCModel> findAllByIdIn(List<String> id);
 }
